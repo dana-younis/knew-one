673c68f77bbcb003c7d9c58ffd326836735b6b1d
dana-younis <younisdana7@gmail.com>
2021-03-29 16:23:01 +0300

    labbb

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # knew-one
+# about me
+* * Hello my name is Dana from irbid, I am a civil engineer, graduated from al Hussein bin tall university and i am 23 years old
---
+++ js/app.js
@@ -0,0 +1,53 @@
+'use strict';
+
+let userName=prompt('what\'s your name?')
+                console.log(userName);
+                alert('hi  ' + userName);
+                let age=prompt('what\'s your age?')
+                console.log(age);
+                alert(userName    + age);
+
+
+let  ex = prompt('do you have software experience?');
+ ex=ex.toLowerCase();
+if (ex=="yes" || ex=="y") { 
+   // console.log("good");}
+   alert("good");}
+else if (ex=="no"||ex=="n") {
+   // console.log("dont worry");} 
+   alert("dont worry");}
+    
+
+let  web =prompt('do you prefer to design complex web bages?');
+ web=web.toLowerCase();
+if (web=="yes" ||web=="y") {alert("it will take time");}
+   // console.log("it will take time");}
+else if (web=="no"||web=="n") {alert ("it will not take time");}
+    //console.log("it will not take time");} 
+
+
+let business =prompt('does  your business depend on these web bages?');
+ business=business.toLowerCase();
+if (business=="yes" ||business=="y") {
+    alert("it will take time");}
+   // console.log("it will take time");}
+else if (business=="no"||business=="n") { alert ("it will not take time");} 
+    //console.log("it will not take time");} 
+
+
+let pay =prompt('can you pay more than 500 dinars? ');
+pay=pay.toLowerCase();
+if (pay=="yes" ||pay=="y") {alert("you will git agreat job");}
+    //console.log("you will git agreat job");}
+else if (pay=="no"||pay=="n") {alert ("iam soory to hear that");}
+    //console.log("iam soory to hear that");} 
+
+
+let work =prompt('do you prefre to work with professionals');
+ work=work.toLowerCase();
+if (work=="yes" ||work=="y") { // console.log("you will get it");}
+    alert("you will get it");}
+   
+else if (work=="no"||work=="n") { alert("our students will help you")};
+    //console.log("our students will help you");} 
+
