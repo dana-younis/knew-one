66d088e69f8c50ecdb318fafbbc66079286d4210
dana-younis <younisdana7@gmail.com>
2021-03-30 21:47:43 +0300

    lab3

--- js/app.js
+++ js/app.js
@@ -1,59 +1,221 @@
 'use strict';
+let userName = prompt("what\'s your name?");
+console.log(userName);
+let age = prompt("what\'s your age?");
+alert("hi " + userName + age);
 
-let userName=prompt('what\'s your name?')
-                console.log(userName);
-                alert('hi  ' + userName);
-                let age=prompt('what\'s your age?')
-                console.log(age);
-                alert(userName    + age);
+let score = 0;
+
+let experience = prompt(" do i have software experience?");
+experience = experience.toLowerCase();
+if (experience == "yes" || experience == "y") { alert("i wish"); }
+else if (experience == "no" || experience == "n") { alert("unfortunately");score++; }
+else { alert(" please choose yes or no"); }
 
 
-let  ex = prompt('do i have software experience?');
- ex=ex.toLowerCase();
-if (ex=="yes" || ex=="y") { 
-   // console.log("good");}
-   alert("you will be surprised");}
-else if (ex=="no"||ex=="n") {
-   // console.log("dont worry");} 
-   alert("dont worry");}
-    
-
-let  web =prompt('do i prefer to design complex web bages?');
- web=web.toLowerCase();
-if (web=="yes" ||web=="y") {alert("it will take time");}
-   // console.log("it will take time");}
-else if (web=="no"||web=="n") {alert ("it will not take time");}
-    //console.log("it will not take time");} 
+let web= prompt("do i prefer to design complex web bages?");
+web=web.toLowerCase();
+if (web == "yes" || web == "y") { alert("it will take time");score++; }
+else if (web == "no" || web == "n") {alert ("it will not take time"); }
+else { alert(" please choose yes or no"); }
 
 
 let business =prompt('does  my  business depend on these web bages?');
  business=business.toLowerCase();
-if (business=="yes" ||business=="y") {
-    alert("yes it is a business");}
-   // console.log("yes it is a business");}
-else if (business=="no"||business=="n") { alert ("you are wrong");} 
-    //console.log("you are wrong");} 
+ if (business == "yes" || business == "y") { alert("yes it is a business"); score++;}
+ else if (business == "no" ||business== "n") {alert ("you are wrong"); }
+ else { alert(" please choose yes or no"); }
 
 
-let pay =prompt("Do you think I am getting more than 200 jd? ");
-pay=pay.toLowerCase();
-if (pay=="yes" ||pay=="y") {alert("you will git agreat job");}
-    //console.log("you will git agreat job");}
-else if (pay=="no"||pay=="n") {alert ("iam soory to hear that");}
-    //console.log("iam soory to hear that");} 
+ let pay =prompt("Do you think I am getting more than 200 jd? ");
+ pay=pay.toLowerCase();
+ if (pay == "yes" || pay == "y") { alert("you will git agreat job");score++; }
+ else if (pay == "no" || pay== "n") {alert("iam soory to hear that"); }
+ else { alert(" please choose yes or no"); }
+
 
 
 let work =prompt('am i professional in my work?');
  work=work.toLowerCase();
-if (work=="yes" ||work=="y") { // console.log("you will get it");}
-    alert("you will get it");}
-   
-else if (work=="no"||work=="n") { alert("our students will help you")};
-    //console.log("our students will help you");} 
+ if (work == "yes" || work== "y") {  alert("you will get it");score++;}
+ else if (work == "no" || work== "n") {alert("our students will help you"); }
+ else { alert(" please choose yes or no"); }
 
+
+
+let number=prompt("what is my preferred number from 1 to 10?");
+ console.log(number);
+let perfer=7;
+for(let i=0;i<5;i++){
+  if (number==perfer) {alert("yes you are right i really prefer it");score++; break; }
+  else if (number>perfer){console.log(number);
+          number=prompt("try again it is higher");}
+  else if (number<perfer){console.log(number);
+    number=prompt("try again it is lower");}}
+
+alert("it was number 7"); 
+   
+
+
+
+  let city= prompt(" what do you think my favorite country?") ;
+  console.log(city);
+  let countries=["Ireland " , "South Africa " ," Canada " ,"Sweden ","Romania","  Italy ","turkey ", "greece "];
+        console.log(countries);
+
+  for(let i=0;i<7;i++){
+      if (countries==city) {console.log( city);
+        alert("i wish i could live there"); score++;break; }
+      else if (city !=countries){console.log( city);
+        city=prompt("try again it is wrong");}}
+        
+
+         alert(countries); 
+         alert(score);
+     
 
     
-   alert ("welcome "+ userName );
 
 
-   
+
+
+
+
+
+
+
+
+
+ 
+// let userName=prompt('what\'s your name?')
+//                 console.log(userName);
+//                 alert('hi  ' + userName);
+//                 let age=prompt('what\'s your age?')
+//                 console.log(age);
+//                 alert(userName    + age);
+
+
+// let  ex = prompt('do i have software experience?');
+//  ex=ex.toLowerCase();
+// if (ex=="yes" || ex=="y") { 
+//    // console.log("good");}
+//    alert("you will be surprised");}
+// else if (ex=="no"||ex=="n") {
+//    // console.log("dont worry");} 
+//    alert("dont worry");}
+
+
+
+// let  web =prompt('do i prefer to design complex web bages?');
+//  web=web.toLowerCase();
+// if (web=="yes" ||web=="y") {alert("it will take time");}
+//    // console.log("it will take time");}
+// else if (web=="no"||web=="n") {alert ("it will not take time");}
+//     //console.log("it will not take time");} 
+
+
+// let business =prompt('does  my  business depend on these web bages?');
+//  business=business.toLowerCase();
+// if (business=="yes" ||business=="y") {
+//     alert("yes it is a business");}
+//    // console.log("yes it is a business");}
+// else if (business=="no"||business=="n") { alert ("you are wrong");} 
+//     //console.log("you are wrong");} 
+
+
+// let pay =prompt("Do you think I am getting more than 200 jd? ");
+// pay=pay.toLowerCase();
+// if (pay=="yes" ||pay=="y") {alert("you will git agreat job");}
+//     //console.log("you will git agreat job");}
+// else if (pay=="no"||pay=="n") {alert ("iam soory to hear that");}
+//     //console.log("iam soory to hear that");} 
+
+
+// let work =prompt('am i professional in my work?');
+//  work=work.toLowerCase();
+// if (work=="yes" ||work=="y") { // console.log("you will get it");}
+//     alert("you will get it");}
+
+// else if (work=="no"||work=="n") { alert("our students will help you")};
+//     //console.log("our students will help you");} 
+
+
+
+
+
+// let num = prompt('what is my preferred number from 1 to 10?')
+// console.log(num);
+// let nop=7;
+
+// for(let i=0;i<5; i++){
+//     if (num==nop) {
+//         alert("you are perfect"); 
+
+//        // score++;
+//         break;}
+
+//     else if (num>nop) { console.log(num); 
+//         num=prompt ("you are too high")
+//       }
+
+//     else if (num<nop){ console.log(num);
+//       num=  prompt(" try again");
+//     }}
+//     alert("it was number 7"); 
+
+
+
+
+
+
+
+
+    // let fav= ["autcade", "proken","html","css","etabs"]
+    // console.log(fav);
+
+    // let favorite =prompt("my favorite computer software or language on computers? ")
+    // for(let i=0;i<7; i++){
+    // if (favorite ==fav) { alert("correct");
+    //     break;
+    // }
+    // else if(favorite !==fav){console.log("favorite")
+    //    favorite= prompt(" try again ")}
+    // }
+
+    //    alert(fav) ;
+
+
+
+
+
+
+// // // while(num!=7){ num=prompt('try again');}
+
+// //     if(num==scoor){alert("you are perfect");
+// // }
+
+// //     else if (num!==scoor) {if (num>7) {alert("you are too high");}
+// //         if (num<scoor) {alert("you are to low");
+
+
+
+
+// // }}
+// // console.log(num)
+
+
+
+// // if (!(num==7)){
+// //     if (num>7){ alert("you are too high");
+// //             }
+// //     if(num<7) {alert("you are to low")} 
+// // }
+// // ifelse (num==7){ alert("you are perfect");
+
+// // } }}
+
+
+
+//    alert ("welcome "+ userName );
+
+
