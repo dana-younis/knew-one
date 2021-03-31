e666447cdd49b8725b50a3f226aa2a284c62f560
dana-younis <younisdana7@gmail.com>
2021-03-31 18:17:11 +0300

    f

--- js/app.js
+++ js/app.js
@@ -1,81 +1,3 @@
-// 'use strict';
-// let userName = prompt("what\'s your name?");
-// console.log(userName);
-// let age = prompt("what\'s your age?");
-// alert("hi " + userName + age);
-// var gopher = 1;
-
-
-
-
-// let score = 0;
-
-
-
-// let experience = prompt(" do i have software experience?");
-// experience = experience.toLowerCase();
-// function software(experience) {
-// if (experience == "yes" || experience == "y") { alert("i wish"); }
-// else if (experience == "no" || experience == "n") { alert("unfortunately");score++; }
-// else { alert(" please choose yes or no"); }
-// return(experience);}
-// software();
-
-
-
-// let web= prompt("do i prefer to design complex web bages?");
-// web=web.toLowerCase();
-// function design(web) {
-// if (web == "yes" || web == "y") { alert("it will take time");score++; }
-// else if (web == "no" || web == "n") {alert ("it will not take time"); }
-// else { alert(" please choose yes or no"); }
-// return(web);}
-// design();
-
-
-
-// let business =prompt('does  my  business depend on these web bages?');
-//  business=business.toLowerCase();
-//  function mony(business) {
-//  if (business == "yes" || business == "y") { alert("yes it is a business"); score++;}
-//  else if (business == "no" ||business== "n") {alert ("you are wrong"); }
-//  else { alert(" please choose yes or no"); }
-//  return(business);}
-//  mony();
-
-
-//  let pay =prompt("Do you think I am getting more than 200 jd? ");
-//  pay=pay.toLowerCase();
-//  function job(pay) {
-//  if (pay == "yes" || pay == "y") { alert("you will git agreat job");score++; }
-//  else if (pay == "no" || pay== "n") {alert("iam soory to hear that"); }
-//  else { alert(" please choose yes or no"); }return(pay);}
-//  job();
-
-
-
-// let work =prompt('am i professional in my work?');
-//  work=work.toLowerCase();
-//  function help(work) {
-//  if (work == "yes" || work== "y") {  alert("you will get it");score++;}
-//  else if (work == "no" || work== "n") {alert("our students will help you"); }
-//  else { alert(" please choose yes or no"); }return(work);}
-//  help();
-
-
-
-// let number=prompt("what is my preferred number from 1 to 10?");
-//  console.log(number);
-// let perfer=7;
-// for(let i=0;i<3;i++){
-//   if (number==perfer) {alert("yes you are right i really prefer it");score++; break; }
-//   else if (number>perfer){console.log(number);
-//           number=prompt("try again it is higher");}
-//   else if (number<perfer){console.log(number);
-//     number=prompt("try again it is lower");}}
-
-// alert("it was number 7"); 
-
 
 'use strict';
 let userName = prompt("what\'s your name?");
@@ -155,37 +77,51 @@ function sign(){
   let perfer=7;
   for(let i=0;i<3;i++){
     if (number==perfer) {alert("yes you are right i really prefer it");score++; break; }
-    else if (number>perfer){console.log(number);
-       number=prompt("try again it is higher");}
-    else if (number<perfer){console.log(number);
-      number=prompt("try again it is lower");}}
-  alert("it was number 7"); 
-  return(number);
-}
-sign();
+    else if (number>perfer){console.log(number);number=prompt("try again it is higher");}
+    else if (number<perfer){console.log(number); number=prompt("try again it is lower");}}
+   alert("it was number 7"); 
+   return(number);
+   }
+   sign();
+
  
 
 
 
 
-function country(){
-  let city=prompt(' what do you think my favorite country?\n Ireland " , "South Africa " ," Canada " ,"Sweden ","Romania","  Italy ","turkey ", "greece' ); 
-  console.log(city);
-  let countries=["Ireland " , "South Africa " ," Canada " ,"Sweden ","Romania","  Italy ","turkey ", "greece "];      
-  // console.log(countries[i]);
-  for(let i=0;i<countries.length;i++){
-      if (countries==city) {console.log( city);
-        alert("i wish i could live there");
-         score++;break; 
-        }else if (city !=countries){
-          console.log(city);      
-         city=prompt("try again it is wrong");
-         alert(countries); 
-         alert('${score}');
-         return(city);
-        }
+
+
+
+function country() {
+  // let city = prompt(' what do you think my favorite country?\n Ireland " , "South Africa " ," Canada " ,"Sweden ","Romania","  Italy ","turkey ", "greece');
+  // console.log(city);
+
+  let countries = ["Ireland ", "South Africa", "Canada", "Sweden ", "Romania", "  Italy ", "turkey ", "greece "];
+  for (let j = 0; j <= 6; j++) {
+   let city = prompt(' what do you think my favorite country?')
+  
+    for (let i = 0; i < countries.length; i++) {
+      if (countries[i] == city) {
+        console.log(countries[i]);
+        console.log(city)
+        ;
+        alert("correct");
+        j=countries.length+2;
+        alert(countries);
+      
+
+        score++;
+        alert(score);
+         break;
+      }
+      
+
+    }
+   
+  }alert("nice");
 }
-}
+
+
 country();
 
 
@@ -193,9 +129,6 @@ country();
 
 
 
-     
-
-    
 
 
 
@@ -207,7 +140,86 @@ country();
 
 
 
- 
+
+
+
+// let userName = prompt("what\'s your name?");
+// console.log(userName);
+// let age = prompt("what\'s your age?");
+// alert("hi " + userName + age);
+// var gopher = 1;
+
+
+
+
+// let score = 0;
+
+
+
+// let experience = prompt(" do i have software experience?");
+// experience = experience.toLowerCase();
+// function software(experience) {
+// if (experience == "yes" || experience == "y") { alert("i wish"); }
+// else if (experience == "no" || experience == "n") { alert("unfortunately");score++; }
+// else { alert(" please choose yes or no"); }
+// return(experience);}
+// software();
+
+
+
+// let web= prompt("do i prefer to design complex web bages?");
+// web=web.toLowerCase();
+// function design(web) {
+// if (web == "yes" || web == "y") { alert("it will take time");score++; }
+// else if (web == "no" || web == "n") {alert ("it will not take time"); }
+// else { alert(" please choose yes or no"); }
+// return(web);}
+// design();
+
+
+
+// let business =prompt('does  my  business depend on these web bages?');
+//  business=business.toLowerCase();
+//  function mony(business) {
+//  if (business == "yes" || business == "y") { alert("yes it is a business"); score++;}
+//  else if (business == "no" ||business== "n") {alert ("you are wrong"); }
+//  else { alert(" please choose yes or no"); }
+//  return(business);}
+//  mony();
+
+
+//  let pay =prompt("Do you think I am getting more than 200 jd? ");
+//  pay=pay.toLowerCase();
+//  function job(pay) {
+//  if (pay == "yes" || pay == "y") { alert("you will git agreat job");score++; }
+//  else if (pay == "no" || pay== "n") {alert("iam soory to hear that"); }
+//  else { alert(" please choose yes or no"); }return(pay);}
+//  job();
+
+
+
+// let work =prompt('am i professional in my work?');
+//  work=work.toLowerCase();
+//  function help(work) {
+//  if (work == "yes" || work== "y") {  alert("you will get it");score++;}
+//  else if (work == "no" || work== "n") {alert("our students will help you"); }
+//  else { alert(" please choose yes or no"); }return(work);}
+//  help();
+
+
+
+// let number=prompt("what is my preferred number from 1 to 10?");
+//  console.log(number);
+// let perfer=7;
+// for(let i=0;i<3;i++){
+//   if (number==perfer) {alert("yes you are right i really prefer it");score++; break; }
+//   else if (number>perfer){console.log(number);
+//           number=prompt("try again it is higher");}
+//   else if (number<perfer){console.log(number);
+//     number=prompt("try again it is lower");}}
+
+// alert("it was number 7"); 
+
 // let userName=prompt('what\'s your name?')
 //                 console.log(userName);
 //                 alert('hi  ' + userName);
@@ -350,7 +362,7 @@ country();
 //   console.log(city);
 
 //   let countries=["Ireland " , "South Africa " ," Canada " ,"Sweden ","Romania","  Italy ","turkey ", "greece "];
-        
+
 //     console.log(countries[i]);
 
 //   for(let i=0;i<5;i++){
@@ -358,7 +370,7 @@ country();
 //         alert("i wish i could live there"); score++;break; }
 //       else if (city !=countries[i]){console.log( city);
 //         city=prompt("try again it is wrong");}}
-        
+
 
 //          alert(countries); 
 //          alert(score);
@@ -370,9 +382,6 @@ country();
 
 
 
-     
-
-    
 
 
 
@@ -384,7 +393,10 @@ country();
 
 
 
- 
+
+
+
+
 // let userName=prompt('what\'s your name?')
 //                 console.log(userName);
 //                 alert('hi  ' + userName);
